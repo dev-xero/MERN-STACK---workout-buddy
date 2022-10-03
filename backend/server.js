812a7414b46f8dc44ad0ec4089e09adba4434c91
CodeNinja-tech <elo48858@gmail.com>
2022-10-03 23:00:41 +0100

    included logger middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+
+app.use((req, res, next) => {
+  console.log(`Request made to the server: ${req.path} ${req.method}`)
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the app' });
-});
+  res.json({ msg: 'Welcome to the app' })
+})
 
 app.listen(port, () => {
   console.log(
     `Listening for requests at port ${port}, site live on: http://localhost:${port}`
-  );
-});
+  )
+})
