4b2911102f87e233007be0a6d4c52d04fca5539c
CodeNinja-tech <elo48858@gmail.com>
2022-10-05 11:37:36 +0100

    connecting frontend to backend

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function NavBar() {
-	return (
-		<header>
-			<Link to="/">
-				<h1>Workout Buddy</h1>
-			</Link>
-		</header>
-	)
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+      </div>
+    </header>
+  )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-	return (
-		<div>Home Page</div>
-	)
+  const [workouts, setWorkouts] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+			const response = await fetch('/api/workouts')
+			console.log(response)
+      const json = await response.json()
+      if (res.ok) {
+        setWorkouts(json)
+      }
+    }
+    fetchWorkouts()
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map(({ _id, title, reps, load }) => (
+						<p key={_id}>{ title }</p>
+          ))}
+      </div>
+    </div>
+  )
 }
